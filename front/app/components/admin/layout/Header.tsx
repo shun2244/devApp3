3c445929ff9bbd0fbccd8a63b0182a7782eb3d50
shinyu <kanazawa1245@gmail.com>
2024-11-25 08:06:30 +0900

    Header 仮作成

---
+++ front/app/components/admin/layout/Header.tsx
@@ -0,0 +1,12 @@
+"use client"
+const Header = () => {
+  return (
+    <header className="border-b flex items-center h-14 px-4">
+      <h1>
+        ss
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
